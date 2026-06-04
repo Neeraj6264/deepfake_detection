e3b57453ca6d151bc451154d965526da4a91b63c
Neeraj6264 <161701889+Neeraj6264@users.noreply.github.com>
2026-06-04 14:55:51 +0530

    Update app.py

--- ai-model/app.py
+++ ai-model/app.py
@@ -6,7 +6,7 @@ from transformers import pipeline
 app = Flask(__name__)
 
 # Load model
-classifier = pipeline("image-classification", model="dima806/deepfake_vs_real_image_detection")
+classifier = pipeline("image-classification", model="deepfake_model.H5/deepfake_vs_real_image_detection")
 
 @app.route("/predict", methods=["POST"])
 def predict():
